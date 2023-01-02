98ef437820fbd1ccad613805675f4ace04a3c662
jimmcgill <jmcgill2a@gmail.com>
2023-01-02 10:23:02 -0600

    Update to properly track and traverse directories.

--- src/main/groovy/jmcgill2/aoc2022/day7/Day7Question1.groovy
+++ src/main/groovy/jmcgill2/aoc2022/day7/Day7Question1.groovy
@@ -4,7 +4,11 @@ class Day7Question1 {
 
     String fileLocation = "//Users//jimmcgill//AdventOfCode2022//Day7//input.txt"
 
-    Directory directory
+    Directory rootDirectory
+
+    Directory currentDirectory
+
+    ArrayList<Directory> allDirectories = []
 
     static void main(String[] args) {
         Day7Question1 q1 = new Day7Question1()
@@ -12,26 +16,49 @@ class Day7Question1 {
     }
 
     public void run() {
-        Directory currentDirectory
 
         int totalSize
         File file = new File(fileLocation)
+        Directory directory
         file.eachLine {line ->
             println line
-            if (line.startsWith("\$ cd") && !(line.startsWith("\$ cd ."))){
-                //println directory + " - " + totalSize
-                String directoryName = line.split(" ")[-1]
-                currentDirectory = new Directory("directoryName")
-                if (directory == null){
-                    directory = currentDirectory
-                } else {
-                    directory.lowerDirectories << currentDirectory
+            if (line.startsWith("\$ cd")){
+                if (line.startsWith("\$ cd ..")){
+                    directory = currentDirectory.previousDirectory
+                    currentDirectory = directory
+                }else {
+                    String directoryName = line.split(" ")[-1]
+                    directory = new Directory(directoryName)
+                    allDirectories << directory
+                    if (rootDirectory == null) {
+                        rootDirectory = directory
+                        currentDirectory = directory
+                        rootDirectory.previousDirectory = directory
+                    } else {
+                        directory.previousDirectory = currentDirectory
+                        currentDirectory.lowerDirectories << directory
+                        currentDirectory = directory
+                    }
                 }
-
             }else if (line[0].isNumber()){
-                totalSize += line.split(" ")[0].toInteger().intValue()
+                int fileSize = line.split(" ")[0].toInteger().intValue()
+                String fileName = line.split(" ")[1]
+                currentDirectory.files << new DirectoryFile(fileName, fileSize)
+                totalSize += fileSize
             }
         }
+        println "root directory size is ${rootDirectory.calculateDirectorySize()}"
         //println directory + " - " + totalSize
+        println "totalSize = $totalSize"
+        def finals = []
+        allDirectories.each{dir ->
+            def size = dir.calculateDirectorySize()
+            if (size <= 100000){
+                finals << [name:dir.directoryName, size:size]
+            }
+        }
+
+        println "finals = $finals"
+        println "size = ${finals.sum{it.size}}"
     }
 }
